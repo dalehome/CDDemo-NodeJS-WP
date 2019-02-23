97b490e92f12d557f86ecfa07cdc82e7a638b208
Dale-Titan <dalemanus@gmail.com>
2019-02-23 10:55:05 +0000

    Version 4 Commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V3 - Buddy' });
+  res.render('index', { title: 'Express V4 - Buddy' });
 });
 
 module.exports = router;
