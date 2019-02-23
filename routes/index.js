9b2d4742bb8b24e21e36f77be167dafc7957d596
Dale-Titan <dalemanus@gmail.com>
2019-02-23 13:04:25 +0000

    Update to Version 8

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V7- All new Bruce the Shark!' });
+  res.render('index', { title: 'Express V8- All new Bollox the Shark!' });
 });
 
 module.exports = router;
