16fac96e9e0d6d6166f85c367023da69f5486d81
dalehome <dale_macdonald@hotmail.com>
2019-02-26 10:41:37 +0000

    Update to version 11

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V10d - All hail Marvin the mighty Monkey!' });
+  res.render('index', { title: 'Express V11 - All hail Marvin the moody Monkey!' });
 });
 
 module.exports = router;
