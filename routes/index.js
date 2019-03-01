25c47423be38e9a995d6dd48cc6b4ce46645616a
dalehome <dale_macdonald@hotmail.com>
2019-03-01 09:59:39 +0000

    Update v 14

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express V13 - All hail Gordo the grumpy Gnat!' });
+  res.render('index', { title: 'Express V14 - All hail Bob the grumpy Gnat!' });
 });
 
 module.exports = router;
